41a97e0129fce70610b4f8630b8d06738dc58e52
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-25 03:27:55 +0100

    Add Db context

---
+++ ExpenseTracker.WebAPI/Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using ExpenseTracker.WebAPI.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExpenseTracker.WebAPI.Data
+{
+    public class AppDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Transaction> Transactions { get; set; }
+        public DbSet<TransactionMethod> TransactionMethods { get; set; }
+        public DbSet<TransactionType> TransactionTypes { get; set; }
+    }
+}
