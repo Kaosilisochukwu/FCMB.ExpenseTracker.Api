00ef30be3932cd30cd6439ff719e7c8956314dc3
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2020-12-25 03:29:09 +0100

    Add migrations and Database

---
+++ ExpenseTracker.WebAPI/Migrations/20201224231438_userUpdate.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ExpenseTracker.WebAPI.Migrations
+{
+    public partial class userUpdate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPublicId",
+                table: "AspNetUsers",
+                maxLength: 20,
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPublicId",
+                table: "AspNetUsers");
+        }
+    }
+}
