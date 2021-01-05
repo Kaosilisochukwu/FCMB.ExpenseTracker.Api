6432711ae367a75a2851b8d1200b37669d689b54
Kaosilisochukwu Nwizu <kcharlse94@gmail.com>
2021-01-05 20:13:03 +0100

    modify applicatioin settings for heroku

--- ExpenseTracker.WebAPI/Program.cs
+++ ExpenseTracker.WebAPI/Program.cs
@@ -16,11 +16,15 @@ namespace ExpenseTracker.WebAPI
             CreateHostBuilder(args).Build().Run();
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            var port = Environment.GetEnvironmentVariable("PORT");
+            return Host.CreateDefaultBuilder(args)
+                    .ConfigureWebHostDefaults(webBuilder =>
+                    {
+                        webBuilder.UseStartup<Startup>()
+                        .UseUrls("http://*:"+port);
+                    });
+        }
     }
 }
--- ExpenseTracker.WebAPI/Startup.cs
+++ ExpenseTracker.WebAPI/Startup.cs
@@ -105,7 +105,7 @@ namespace ExpenseTracker.WebAPI
 
             //app.UseCors(x => x.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
 
-            app.UseHttpsRedirection();
+            //app.UseHttpsRedirection();
 
             app.UseRouting();
 
